1ab4db5ec892fd6b5728554e53acd14e29309aeb
ahoora79 <ahooraxx@gmail.com>
2024-04-30 05:18:00 +0330

    handle Post method Request and Testing with Postman

--- webserver/main.cpp
+++ webserver/main.cpp
@@ -8,9 +8,11 @@
 #include <QUrl>
 #include <Qfile>
 #include <QTextStream>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 const QString apiService = "/service";
-const int port = 10051;
+const int port = 80;
 const QString namesFile = "name.csv";
 
 
@@ -35,6 +37,28 @@ public:
         return Content;
     }
 
+    QString buildResponse(QString strResource,QByteArray requestData){
+        if( QString::fromUtf8(requestData)[0] == 'G') // GET Method
+            return strResource; //return users
+        else { // POST Method
+            QString reqString = QString::fromUtf8(requestData);
+
+
+            int jsonStartIndex = reqString.indexOf("{");
+            QString jsonString = reqString.mid(jsonStartIndex);
+
+            QJsonDocument doc = QJsonDocument::fromJson(jsonString.toUtf8());
+            QJsonObject json = doc.object();
+            QString username = json["username"].toString();
+
+
+            if(strResource.contains(username,Qt::CaseSensitive))
+                strResource = "Success";
+            else
+                strResource = "Failed!";
+            return strResource;
+        }
+    }
 protected:
     void incomingConnection(qintptr socketDescriptor) override
     {
@@ -51,13 +75,14 @@ protected:
             QString path = firstLineParts.at(1);
 
             QString strResource = readFileContents("../../assets/" + namesFile);
-            QString lenght = QString(std::to_string(strResource.length()).c_str());
+            QString response = buildResponse(strResource,requestData);
+            QString lenght = QString(std::to_string(response.length()).c_str());
             if (path == apiService) {
                 QByteArray responseData = "HTTP/1.1 200 OK\r\n"
                                           "Content-Type: text/plain\r\n"
                                           "Content-Length: " + lenght.toUtf8() + "\r\n"
                                                               "\r\n" +
-                                          strResource.toUtf8();
+                                          response.toUtf8();
                 socket->write(responseData);
                 socket->flush();
                 socket->waitForBytesWritten();
